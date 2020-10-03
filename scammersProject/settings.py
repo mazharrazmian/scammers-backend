d06a588bd339b7ed530047ccac71ddfe8b18f930
unknown <mazhar.ali1996@yahoo.com>
2020-10-03 21:18:28 +0500

    Changed allowed hosts settings

--- scammersProject/settings.py
+++ scammersProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://scammers-backend.herokuapp.com/']
+ALLOWED_HOSTS = ['scammers-backend.herokuapp.com','127.0.0.1']
 
 AUTH_USER_MODEL = 'scammers.User'
 # Application definition
